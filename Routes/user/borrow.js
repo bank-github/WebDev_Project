caba2d052803783e37b0c1b7f6f357d051e2089a
Viramon110 <149892063+Viramon110@users.noreply.github.com>
2023-11-12 21:12:51 +0700

    borrow (unfinished)

--- Routes/user/borrow.js
+++ Routes/user/borrow.js
@@ -3,8 +3,24 @@ const router = express.Router();
 const path = require('path');
 const con = require('../../config/db');
 
-router.get('/borrow',function(req,res){
-    res.sendFile(path.join(__dirname,'../../views/user/borrow.html'));
+router.get('/borrow', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../views/user/borrow.html'));
 })
+router.post('/borrow', function (req, res) {
+    const borrows = { borrow_id, borrow_date, borrow_time, return_date, return_time, status, message, user_id, asset_id, admin_name } = req.body;
+    const sql = `INSERT INTO borrow (borrow_id,borrow_date,borrow_time, return_date, return_time, status, message, user_id, asset_id, admin_name) VALUES (?,?,?,?,?,?,?,?,?,?)`;
+    con.query(sql, borrows, function (err, results) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Database server error");
+        }
+        if (results.affectedRows != 1) {
+            console.error('Row added is not 1');
+            return res.status(500).send("Add failed");
+        }
+        res.send('/user/list');
+    });
+
+});
 
 module.exports = router;
