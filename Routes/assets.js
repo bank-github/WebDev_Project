4f51100ce29cf260f6e9e7339dcdb3bb5ac182b0
nuenqxr <6431501123@lamduan.mfu.ac.th>
2023-11-13 15:28:06 +0700

    ADD-Assets

--- Routes/assets.js
+++ Routes/assets.js
@@ -11,5 +11,25 @@ router.get('/assets', function (req, res) {
         res.status(200).send(result);
     })
 });
+router.post('/add-assets',function (req,res){
+    const asset_id = req.params.asset_id;
+    const update = req.body;
+    const query = `INSERT INTO assets SET asset_name = ?,detail=?,status = 1,image = ? `; 
+    con.query(query, [update.asset_name,update.detail,update.image] , function (err,result) {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Database server error');
+        }else if(result.affectedRows != 1){
+            console.error('Row update is not 1');
+            return res.status(500).send('Update failed');
+
+        }else{
+            res.send('Update succesfully');
+        }
+        
+    })
+})
+
+
 
 module.exports = router;
