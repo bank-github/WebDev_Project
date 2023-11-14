09ba0d0c8ce8a36fa4b540ba9c7215c321d29af5
bank-github <6431501089@lamduan.mfu.ac.th>
2023-11-14 21:38:40 +0700

    last update 14112003

--- Routes/assets.js
+++ Routes/assets.js
@@ -30,8 +30,8 @@ router.delete('/assets/:id', function (req, res) {
         })
     })
 })
-router.put('/edit-assets/:asset_id', function (req, res) {
-    const asset_id = req.params.asset_id;
+router.put('/assets/:id', function (req, res) {
+    const asset_id = req.params.id;
     // res.send(asset_id)
     const { asset_name, detail, asset_status, image } = req.body;
     const sql = "UPDATE assets SET asset_name = ?, detail = ?, asset_status = ?, image = ? WHERE assets.asset_id = ? ;"
@@ -48,26 +48,7 @@ router.put('/edit-assets/:asset_id', function (req, res) {
     });
 
 });
-router.delete('/assets/:id', function (req, res) {
-    const id = req.params.id;
-    const sqlbr = `DELETE FROM borrow WHERE asset_id = ?`;
-    con.query(sqlbr, [id], function (err, result) {
-        if (err) {
-            console.error(err);
-            return res.status(500).send('Database Error!');
-        }
-        const sql = `DELETE FROM assets WHERE asset_id = ?`;
-        con.query(sql, [id], function (err, result) {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Database Error!');
-            }
-            res.send('Deleted!');
-        })
-    })
-});
-router.post('/add-assets',function (req,res){
-    const asset_id = req.params.asset_id;
+router.post('/assets',function (req,res){
     const update = req.body;
     const query = `INSERT INTO assets SET asset_name = ?,detail=?,asset_status = 1,image = ? `; 
     con.query(query, [update.asset_name,update.detail,update.image] , function (err,result) {
