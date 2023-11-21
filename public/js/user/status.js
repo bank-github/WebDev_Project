0d917ec1fb5627edbecb14c52b15bba178682f79
bank-github <6431501089@lamduan.mfu.ac.th>
2023-11-21 10:31:54 +0700

    update user status

--- public/js/user/status.js
+++ public/js/user/status.js
@@ -1,27 +1,83 @@
+// for button status
+const pending = document.querySelector('#pending');
+const approve = document.querySelector('#approve');
+const reject = document.querySelector('#reject');
+var borrowUser;
+// use when error or success
+const allList = document.querySelector('#all-list');
+const userID = window.localStorage.getItem('userID');
+
+// pending onclick
+pending.onclick = function () {
+  pending.disabled = true;
+  approve.disabled = false;
+  reject.disabled = false;
+  // default pending shown
+  const brPending = borrowUser.filter((br) => br.status == 1);
+  listCreate(brPending);
+}
+// appreve onclick
+approve.onclick = function () {
+  pending.disabled = false;
+  approve.disabled = true;
+  reject.disabled = false;
+  // default pending shown
+  const brApprove = borrowUser.filter((br) => br.status == 2 || (br.status == 5 && br.update_status == null));
+  listCreate(brApprove);
+}
+// pending onclick
+reject.onclick = function () {
+  pending.disabled = false;
+  approve.disabled = false;
+  reject.disabled = true;
+  // default pending shown
+  const brReject = borrowUser.filter((br) => br.status == 3);
+  // kept only 5 element
+  if (brReject.length > 5) {
+    listCreate(brReject.slice(0, 5));
+  } else {
+    listCreate(brReject);
+  }
+
+}
+
 // get all asset
 async function getList() {
-  // use when error or success
-  const allList = document.querySelector('#all-list');
-  const userID = window.localStorage.getItem('userID');
   // console.log('allList Element:', allList);
   try {
     const result = await fetch('/borrows');
     // console.log('Server Response:', result);
     if (result.ok) {
       const data = await result.json();
-      const borrowUser = data.filter((dt) => dt.user_id == userID);
-      let content = '';
-      if (borrowUser.length > 0) {
-        borrowUser.forEach(borrow => {
-          const formatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-          const borrowDate = new Date(borrow.borrow_date).toLocaleString(undefined, formatOptions);
-          const returnDate = new Date(borrow.return_date).toLocaleString(undefined, formatOptions);
-          const color = colorStatus(borrow.status);
-          const status = textStatus(borrow.status);
-          if (borrow.status != 4 && (borrow.status != 5 || borrow.update_status == null)) {
-            content += `
+      borrowUser = data.filter((dt) => dt.user_id == userID);
+      // default pending shown
+      const brPending = borrowUser.filter((br) => br.status == 1);
+      listCreate(brPending);
+
+    } else {
+      const data = await result.text();
+      allList.innerHTML = data;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// function create status list
+function listCreate(borrowUser) {
+  let content = '';
+  if (borrowUser.length > 0) {
+    borrowUser.forEach(borrow => {
+      const formatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+      const borrowDate = new Date(borrow.borrow_date).toLocaleString(undefined, formatOptions);
+      const returnDate = new Date(borrow.return_date).toLocaleString(undefined, formatOptions);
+      const color = colorStatus(borrow.status);
+      const status = textStatus(borrow.status);
+
+      // borrow.status != 4 && (borrow.status != 5 || borrow.update_status == null)
+      content += `
           <div class="d-flex">
-            <div class="col-9 border border-3 border-dark rounded-pill m-3 p-2 bg-white shadow">
+            <div class="col-9 border border-3 border-dark rounded-pill m-3 mb-3 p-2 bg-white shadow">
               <div class="row">
                   <div class="col-4">
                       <h4 class="pt-1 ps-5">Asset: ${borrow.asset_name}</h4>
@@ -39,24 +95,16 @@ async function getList() {
               <h4 class="text-center pt-1"  ${color}>${status}</h4>
             </div>
           </div>`;
-          }
-        });
-      }
-      else {
-        content = `<hr><h1 class="text-center">Has no borrow assets</h1>`;
-      }
-      // if borrow has data but has no status 1
-      if (content == '') {
-        content = `<hr><h1 class="text-center">Has no borrow assets</h1>`;
-      }
-      allList.innerHTML = content;
-    } else {
-      const data = await result.text();
-      allList.innerHTML = data;
-    }
-  } catch (err) {
-    console.error(err);
+    });
   }
+  else {
+    content = `<hr><h1 class="text-center">Has no assets</h1>`;
+  }
+  // if borrow has data but has no status 1
+  if (content == '') {
+    content = `<hr><h1 class="text-center">Has no assets</h1>`;
+  }
+  allList.innerHTML = content;
 }
 
 
